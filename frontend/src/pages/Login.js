22c0a8e1a27001993b8117b5f55dbd0c43409e74
Ameen <72974478+AAjayiB@users.noreply.github.com>
2024-02-13 00:07:06 +0000

    Add files via upload

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Login = () => {
+    return (
+        <div className='Login'>
+            <Header />
+            <h2>Login Page</h2>
+        </div>
+    );
+};
+
+export default Login;
