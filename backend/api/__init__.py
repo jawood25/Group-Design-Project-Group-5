e5e057a6d0806440eb7601f886d24b2150acc752
Yuzhe <ywu6@tcd.ie>
2024-02-15 12:20:19 +0000

    add comments for backend code

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,23 +1,26 @@
-import certifi
 from flask import Flask
 from flask_cors import CORS
 from .exts import init_exts
-from .routes import *
 
-
+# Function to create and configure the Flask app
 def create_apis():
+    # Initialize the Flask app
     app = Flask(__name__)
+    # Enable CORS for all domains on all routes
     CORS(app)
 
+    # Configure MongoDB settings for the Flask app
     app.config['MONGODB_SETTINGS'] = {
-        'db': 'pathpal',
-        'host': 'mongodb+srv://cluster0.b6yu9ji.mongodb.net',
-        'username': 'PathPalAdmin',
-        'password': 'YOAybG23XVTqQnri',
-        'retryWrites': True,
-        'w': 'majority'
+        'db': 'pathpal',  # Name of the database
+        'host': 'mongodb+srv://cluster0.b6yu9ji.mongodb.net',  # MongoDB Atlas cluster URL
+        'username': 'PathPalAdmin',  # Username for MongoDB
+        'password': 'YOAybG23XVTqQnri',  # Password for MongoDB
+        'retryWrites': True,  # Enable retryable writes
+        'w': 'majority'  # Write concern set to "majority" for data integrity
     }
 
+    # Call the init_exts function to initialize Flask extensions with the app context
     init_exts(app=app)
 
+    # Return the configured Flask app
     return app
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,49 +1,88 @@
-from datetime import datetime
-from .exts import db
 
+
+from datetime import datetime
+from .exts import db  # Importing the database instance from an external module
+
+
+# Defines a User document with various fields to store user information
 class User(db.Document):
-    username = db.StringField(required=True,unique=True)
+    # Unique username required for each user
+    username = db.StringField(required=True, unique=True)
+    # Password field, required for each user
     password = db.StringField(required=True)
+    # Optional email field for the user
+    email = db.StringField()
+    # Optional name field for the user
+    name = db.StringField()
+    # Optional age field for the user
+    age = db.IntField()
+    # Optional phone field, must be unique if provided
+    phone = db.IntField(unique=True)
+    # List of references to other User documents representing friends, auto-updates if a friend is deleted
+    friends = db.ListField(db.ReferenceField('self', reverse_delete_rule='PULL'))
+
+    ### code for further use ###
 
     # email = db.StringField(required=True,primary_key=True)
     # name = db.StringField(required=True)
-    #age = db.IntField()
-    #phone = db.IntField(unique=True)
-    #friends = db.ListField(db.ReferenceField('self', reverse_delete_rule='PULL'))
+    # age = db.IntField()
+    # phone = db.IntField(unique=True)
+    # friends = db.ListField(db.ReferenceField('self', reverse_delete_rule='PULL'))
 
+
+# Defines a Comment document associated with users and their interactions
 class Comment(db.Document):
+    # Reference to a User document representing the comment's author, required
     author = db.ReferenceField(User, required=True)
-
+    # Date and time when the comment was posted, defaults to the current UTC time
     date_posted = db.DateTimeField(default=datetime.utcnow)
-
+    # Number of dislikes for the comment, defaults to 0
     dislikes = db.IntField(default=0)
+    # Number of likes for the comment, defaults to 0
     likes = db.IntField(default=0)
-
+    # The body of the comment, required
     body = db.StringField(required=True)
 
+
+# Defines a Route document for storing information about specific routes
 class Route(db.Document):
+    # Required starting point of the route
     startPoint = db.StringField(required=True)
+    # Optional description of the route
     description = db.StringField()
-
+    # Number of dislikes for the route, defaults to 0
     dislike = db.IntField(default=0)
+    # Number of likes for the route, defaults to 0
     like = db.IntField(default=0)
+    # Number of saves for the route, indicating how many users have saved it, defaults to 0
     saves = db.IntField(default=0)
+    # Optional field for the time taken to complete the route
     time_taken = db.IntField()
+    # Optional field for the distance of the route
     distance = db.IntField()
-
+    # Reference to the User document of the route's creator, required
     creator = db.ReferenceField(User, required=True)
+    # Optional reference to a Comment document, auto-updates if the comment is deleted
     comment = db.ReferenceField(Comment, reverse_delete_rule='PULL')
 
+
+# Defines a UserEvent document for events created by users
 class UserEvent(db.Document):
-    name = db.StringField(required=True,)
+    # Required name of the event
+    name = db.StringField(required=True, )
+    # Required host of the event
     host = db.StringField(required=True)
+    # Required venue of the event
     venue = db.StringField(required=True)
-
+    # Date and time of the event, defaults to the current UTC time
     date = db.DateTimeField(default=datetime.utcnow)
-
-    route = db.ReferenceField(Route,required=True)
-
+    # Required reference to a Route document associated with the event
+    route = db.ReferenceField(Route, required=True)
+    # Number of users interested in the event, defaults to 0
     interested = db.IntField(default=0)
+
+
+### code for further use ###
 
 # class FriendList(Document):
 #     number_of_friends = IntField(default=0)
@@ -56,4 +95,3 @@ class UserEvent(db.Document):
 # class RouteList(Document):
 #     number_of_saved_routes = IntField(default=0)
 #     routes = ListField(ReferenceField('Route'))
-
