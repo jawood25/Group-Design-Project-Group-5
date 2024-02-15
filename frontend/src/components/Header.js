a9af423e34c9bee36247e215456b409b04f1dda8
Moeto88 <sonodam@tcd.ie>
2024-02-15 19:09:14 +0000

    change class to className

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,20 +6,20 @@ const Header = () => {
     const isLoggedIn = useSelector((state) => state.loginStaus.isLoggedIn);
     return (
         <div className="Header">
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <div class="d-flex">
-                        <div class="dropdown">
-                            <button class="navbar-toggler" type="button" data-bs-toggle="dropdown" aria-expanded="false"><span class="navbar-toggler-icon"></span></button>
-                            <ul class="dropdown-menu">
-                                <li><button class="dropdown-item" type="button">Menu1</button></li>
-                                <li><button class="dropdown-item" type="button">Menu2</button></li>
-                                <li><button class="dropdown-item" type="button">Menu3</button></li>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <div className="d-flex">
+                        <div className="dropdown">
+                            <button className="navbar-toggler" type="button" data-bs-toggle="dropdown" aria-expanded="false"><span className="navbar-toggler-icon"></span></button>
+                            <ul className="dropdown-menu">
+                                <li><button className="dropdown-item" type="button">Menu1</button></li>
+                                <li><button className="dropdown-item" type="button">Menu2</button></li>
+                                <li><button className="dropdown-item" type="button">Menu3</button></li>
                             </ul>
                         </div>
-                        <Link to="/" class="navbar-brand ms-2">PathPal</Link>
+                        <Link to="/" className="navbar-brand ms-2">PathPal</Link>
                     </div>
-                    <div class="d-flex">
+                    <div className="d-flex">
                     {isLoggedIn ? (
                             <Link to="/my-account" className="btn btn-info text-white me-2">My Account</Link>
                         ) : (
