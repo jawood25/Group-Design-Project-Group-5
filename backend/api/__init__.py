6a97a5c2409cb5efd9ba836c5b601fa70456c156
Ameen <72974478+AAjayiB@users.noreply.github.com>
2024-02-16 13:47:01 +0000

    fixed import orders

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,7 +1,7 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
-from dotenv import load_dotenv
-import os
 from .exts import init_exts
 from .routes import *
 
