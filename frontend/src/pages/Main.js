985302c4fca5edd4abdb60a7b5c0675d106cfca5
Moeto88 <sonodam@tcd.ie>
2024-02-28 16:13:37 +0000

    delete testmap page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,12 +1,10 @@
 import Header from '../components/Header';
-import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div className='Main'>
             <Header />
             <h2>Main Page</h2>
-            <Link to="/test-map" className="btn btn-info text-white me-2">Test Rendering Map</Link>
         </div>
     );
 };
