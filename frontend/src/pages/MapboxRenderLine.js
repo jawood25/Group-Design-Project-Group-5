93491d8143e5ecd428afffbb9345887ea010cc86
Moeto88 <sonodam@tcd.ie>
2024-03-10 19:54:48 +0000

    update my account page

--- frontend/src/pages/MapboxRenderLine.js
+++ frontend/src/pages/MapboxRenderLine.js
@@ -4,7 +4,7 @@ import "../style/map.css";
 
 mapboxgl.accessToken = "";
 
-const MapboxRenderLine = () => {
+const MapboxRenderLine = (route) => {
     const mapContainerRef = useRef(null);
 
   // Initialize map when component mounts
@@ -12,7 +12,7 @@ const MapboxRenderLine = () => {
         const map = new mapboxgl.Map({
             container: mapContainerRef.current,
             style: 'mapbox://styles/mapbox/streets-v12',
-            center: [-122.486052, 37.830348],
+            center: [route.route.map_center.lng, route.route.map_center.lat],
             zoom: 14,
         });
 
@@ -24,29 +24,7 @@ const MapboxRenderLine = () => {
                     properties: {},
                     geometry: {
                         type: 'LineString',
-                        coordinates: [
-                            [-122.483696, 37.833818],
-                            [-122.483482, 37.833174],
-                            [-122.483396, 37.8327],
-                            [-122.483568, 37.832056],
-                            [-122.48404, 37.831141],
-                            [-122.48404, 37.830497],
-                            [-122.483482, 37.82992],
-                            [-122.483568, 37.829548],
-                            [-122.48507, 37.829446],
-                            [-122.4861, 37.828802],
-                            [-122.486958, 37.82931],
-                            [-122.487001, 37.830802],
-                            [-122.487516, 37.831683],
-                            [-122.488031, 37.832158],
-                            [-122.488889, 37.832971],
-                            [-122.489876, 37.832632],
-                            [-122.490434, 37.832937],
-                            [-122.49125, 37.832429],
-                            [-122.491636, 37.832564],
-                            [-122.492237, 37.833378],
-                            [-122.493782, 37.833683]
-                        ]
+                        coordinates: route.route.coordinates
                     }
                 },
             });
@@ -60,11 +38,16 @@ const MapboxRenderLine = () => {
                     'line-cap': 'round'
                 },
                 paint: {
-                    'line-color': '#888',
-                    'line-width': 8
+                    'line-color': '#F44336',
+                    'line-width': 3
                 }
             });
         });
+
+        return () => {
+            map.remove();
+        };
+
     }, []);
 
     return <div className="map-container" ref={mapContainerRef} />;
--- frontend/src/pages/MyAccount.js
+++ frontend/src/pages/MyAccount.js
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
 import { useSelector } from 'react-redux';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import MapboxRenderLine from './MapboxRenderLine';
 
 const MyAccount = () => {
     const username = useSelector((state) => state.userInfo.username)
+    const [routeData, setRouteData] = useState(null);
 
     useEffect(() => {
         const fetchUserRoutes = async () => {
@@ -20,6 +22,7 @@ const MyAccount = () => {
                 }
                 const data = await response.json();
                 console.log(data)
+                setRouteData(data.routes)
             } catch (error) {
                 console.error('There was a problem with your fetch operation:', error);
             }
@@ -33,6 +36,19 @@ const MyAccount = () => {
             <Header />
             <h2>My Account Page</h2>
             <h3>Username: {username}</h3>
+            {routeData && routeData.map((route, index) => (
+                <div className="row">
+                    <div className="col">
+                        <MapboxRenderLine route={route} />
+                    </div>
+                    <div className="col">
+                        <div>City: {route.city}</div>
+                        <div>Location: {route.location}</div>
+                        <div>Time: {route.hours}:{route.minutes}</div>
+                        <div>Difficulty: {route.difficulty}</div>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
