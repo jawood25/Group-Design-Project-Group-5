dc9cce439274cc3e022c531deb515d1c68eecbce
Moeto88 <sonodam@tcd.ie>
2024-03-13 19:06:38 +0000

    main map added

--- frontend/src/components/MainMap.js
+++ frontend/src/components/MainMap.js
@@ -3,101 +3,92 @@ import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = "";
 
-const MainMap = () => {
+const MainMap = ({allUR}) => {
     const mapContainerRef = useRef(null);
+    const colorList = ["#F44336", "#E91E63", "#9C27B0", "#3F51B5", "#2196F3", "#00BCD4", "#009688", "#FFC107", "#FF9800"]
 
   // Initialize map when component mounts
     useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [-6.255069, 53.343575],
-            zoom: 12,
-        });
+        if (allUR) {
+            const map = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: 'mapbox://styles/mapbox/streets-v12',
+                center: [-6.255069, 53.343575],
+                zoom: 13,
+            });
+    
+            map.on("load", function () {
+    
+                Object.keys(allUR).forEach(key => {
+                    if((allUR[key]).length > 0) {
+                        console.log(key)
+                        allUR[key].forEach((route) => {
+                            console.log(route)
 
-        map.on("load", function () {
-            map.addSource("route", {
-                type: "geojson",
-                data: {
-                    type: "Feature",
-                    properties: {},
-                    geometry: {
-                        type: 'LineString',
-                        coordinates: [
-                            [
-                                -6.228183334329287,
-                                53.344557704957396
-                            ],
-                            [
-                                -6.215259057891529,
-                                53.342851662215025
-                            ],
-                            [
-                                -6.212791105607238,
-                                53.34118432721914
-                            ],
-                            [
-                                -6.219805285784304,
-                                53.337422890255255
-                            ],
-                            [
-                                -6.226364843172405,
-                                53.341533309705966
-                            ],
-                            [
-                                -6.227988495990843,
-                                53.34254146529159
-                            ],
-                            [
-                                -6.228053442104056,
-                                53.3444801590407
-                            ]
-                        ]
+                            const sourceId = `route-${route.id}`;
+                            const layerId = `route-${route.id}-layer`;
+
+
+                            map.addSource(sourceId, {
+                                type: "geojson",
+                                data: {
+                                    type: "Feature",
+                                    properties: {},
+                                    geometry: {
+                                        type: 'LineString',
+                                        coordinates: route.coordinates
+                                    }
+                                },
+                            });
+
+                            const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
+                            // Add a symbol layer
+                            map.addLayer({
+                                id: layerId,
+                                type: "line",
+                                source: sourceId,
+                                layout: {
+                                    'line-join': 'round',
+                                    'line-cap': 'round'
+                                },
+                                paint: {
+                                    'line-color': randomColor,
+                                    'line-width': 4
+                                }
+                            });
+                            // Add click event listener to the line layer
+                            map.on("click", layerId, function (e) {
+                                const coordinates = e.lngLat;
+                                const description = `<strong style="font-size: 16px; padding: 0 5px;">Route by ${route.creator_username} </strong><p style="margin-bottom: 5px;"></p>
+                                                    <p style="font-size: 14px; margin-bottom: 0; padding: 0 5px;">Distance: ${route.distance} km</p>
+                                                    <p style="font-size: 14px; margin-bottom: 0; padding: 0 5px;">${route.mobility}: ${route.minutes} minutes</p>
+                                                    <p style="font-size: 14px; margin-bottom: 0; padding: 0 5px;">Difficulty: ${route.difficulty}</p>
+                                                    `;
+                                new mapboxgl.Popup()
+                                    .setLngLat(coordinates)
+                                    .setHTML(description)
+                                    .addTo(map);
+                            });
+                
+                            // Change the cursor to a pointer when hovering over the line layer
+                            map.on("mouseenter", layerId, function () {
+                                map.getCanvas().style.cursor = "pointer";
+                            });
+                
+                            // Change it back to the default cursor when it leaves
+                            map.on("mouseleave", layerId, function () {
+                                map.getCanvas().style.cursor = "";
+                            });
+                        })
                     }
-                },
+                });
             });
-            // Add a symbol layer
-            map.addLayer({
-                id: "route",
-                type: "line",
-                source: "route",
-                layout: {
-                    'line-join': 'round',
-                    'line-cap': 'round'
-                },
-                paint: {
-                    'line-color': '#F44336',
-                    'line-width': 3
-                }
-            });
-
-            // Add click event listener to the line layer
-            map.on("click", "route", function (e) {
-                const coordinates = e.lngLat;
-                const description = "This is your route.";
-                new mapboxgl.Popup()
-                    .setLngLat(coordinates)
-                    .setHTML(description)
-                    .addTo(map);
-            });
-
-            // Change the cursor to a pointer when hovering over the line layer
-            map.on("mouseenter", "route", function () {
-                map.getCanvas().style.cursor = "pointer";
-            });
-
-            // Change it back to the default cursor when it leaves
-            map.on("mouseleave", "route", function () {
-                map.getCanvas().style.cursor = "";
-            });
-            
-        });
-
-        return () => {
-            map.remove();
-        };
-
-    }, []);
+    
+            return () => {
+                map.remove();
+            };
+        }
+    }, [allUR]);
 
     return <div className="map-container" ref={mapContainerRef} style={{ width: "100%", height: "100vh" }}/>;
 };
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,35 @@
 import Header from '../components/Header';
 import MainMap from '../components/MainMap';
+import React, { useEffect, useState } from 'react';
 
 const Main = () => {
+    const [allUR, setAllUR] = useState(null);
+    useEffect(() => {
+        const fetchUserRoutes = async () => {
+            try {
+                const response = await fetch('/api/allUR', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setAllUR(data.user_routes)
+            } catch (error) {
+                console.error('There was a problem with your fetch operation:', error);
+            }
+        };
+
+        fetchUserRoutes();
+    }, []);
+
     return (
         <div className='Main'>
             <Header />
-            <MainMap />
+            <MainMap allUR={allUR}/>
         </div>
     );
 };
