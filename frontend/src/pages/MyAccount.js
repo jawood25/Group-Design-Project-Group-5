8b90d5f2460a7e17c349fe7136d9d973a16da08c
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2024-03-14 21:40:00 +0100

    basic route search in account

--- frontend/src/pages/MyAccount.js
+++ frontend/src/pages/MyAccount.js
@@ -1,13 +1,37 @@
+// MyAccount.js
 import Header from '../components/Header';
 import { useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import MapboxRenderLine from './MapboxRenderLine';
 import '../style/myaccount.css'
+import RouteSearch from '../components/RouteSearch';
 
 const MyAccount = () => {
     const username = useSelector((state) => state.userInfo.username)
     const [routeData, setRouteData] = useState(null);
 
+    const handleSearch = async (searchParams) => {
+        try {
+            const response = await fetch('/api/searchroute', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(searchParams),
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log(data)
+            data.routes = data.routes.filter(route => route.creator_username === username)
+            setRouteData(data.routes);
+        } catch (error) {
+            console.error('There was a problem with your fetch operation:', error);
+        }
+    };
+
+    
     useEffect(() => {
         const fetchUserRoutes = async () => {
             try {
@@ -35,11 +59,12 @@ const MyAccount = () => {
     return (
         <div className='MyAccount'>
             <Header />
+            <RouteSearch onSearch={handleSearch} />
             <h3>Username: {username}</h3>
             <h2>My Account Page</h2>
             <div className="grid-container">
                 {routeData && routeData.map((route, index) => (
-                    <div className="grid-item">
+                    <div className="grid-item" key={index}>
                         <MapboxRenderLine route={route} />
                         <div className="info">
                             <div><b>City:</b>  {route.city}</div>
@@ -47,6 +72,8 @@ const MyAccount = () => {
                             <div><b>Distance:</b>  {route.distance}km</div>
                             <div><b>Time:</b>  {route.hours}:{route.minutes}</div>
                             <div><b>Difficulty:</b>  {route.difficulty}</div>
+                            <div><b>Mobility:</b>  {route.mobility}</div>
+                            <div><b>Comment:</b>  {route.comment}</div>
                         </div>
                     </div>
                 ))}
